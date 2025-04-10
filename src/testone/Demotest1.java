708b2e95ecd671253a8cf88ccf1c528f242dfb48
Ganesh <vmwms99@gmail.com>
2025-04-10 17:11:38 +0530

    First commit.....

---
+++ src/testone/Demotest1.java
@@ -0,0 +1,45 @@
+package testone;
+
+public class Demotest1 {
+
+public static void main(String[] args) 
+	
+	{
+		// Variable declaration and assign values
+		
+		String MemName;
+		MemName="Arjun Patil";
+		
+		int MemID;
+		MemID=125;
+		
+		int MemAge;
+		MemAge=27;
+		
+		float Memhght;
+		Memhght=5.7f;
+		
+		double MemWght;
+		MemWght=58.45;
+		
+		char MemGrade;
+		MemGrade= 'A';
+		
+		boolean Premstatus;
+		Premstatus=true;
+		
+		//Printing Statements
+		
+		System.out.println("Gym Member Information:");
+		System.out.println("--------------------------------");
+		System.out.println("Name of the Member is: " + MemName);
+		System.out.println("ID of the Member is: " + MemID);
+		System.out.println("Age of the Member is: " + MemAge +" Years");
+		System.out.println("Height of the Member is: " + Memhght +" Feet"); 
+		System.out.println("Weight of the Member is: " + MemWght+" Kg");
+		System.out.println("Grade of the Member is: " + MemGrade);
+		System.out.println("Premium Membership Status: " + Premstatus);
+		
+	}
+
+}
