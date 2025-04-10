237bf55b607cc89ade3a298a28670ad6401129e5
vaibhav <realvmw@gmail.com>
2025-04-10 19:33:10 +0530

    Added Comments

--- src/testone/Demotest2.java
+++ src/testone/Demotest2.java
@@ -2,16 +2,18 @@ package testone;
 
 public class Demotest2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // main method 
 		
+		//integers declarations 
 		int a =10;
 		int  b =20; 
 		int   c;
 		
-		c=a+b;
+		c=a+b; // formulas  
 		
-		System.out.println("addition of num : "+c);
+		System.out.println("addition of num : "+c); // printing 
 
 	}
 
 }
+	
