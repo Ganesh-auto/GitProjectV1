f6346f3864bbd1a09fc9e05b6d20a45124c16097
vaibhav <realvmw@gmail.com>
2025-04-10 18:55:12 +0530

    Committing Demotest2 to BranchX

---
+++ src/testone/Demotest2.java
@@ -0,0 +1,17 @@
+package testone;
+
+public class Demotest2 {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int  b =20; 
+		int   c;
+		
+		c=a+b;
+		
+		System.out.println("addition of num : "+c);
+
+	}
+
+}
